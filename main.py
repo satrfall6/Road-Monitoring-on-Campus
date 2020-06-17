cbcb93a0286298d69e200cf22b3a0a77cfaa651d
satrfall6 <satrfall6@gmail.com>
2020-06-17 10:47:06 +1000

    UPDATE:fix bugs when using different grid size; add ROI

--- main.py
+++ main.py
@@ -12,15 +12,17 @@ import cv2
 print(cv2.getBuildInformation())
 
 import os 
-#os.chdir(r'C:\Users\satrf\LB_stuff\Git\Road-Monitoring-on-Campus')
+os.chdir(r'C:\Users\satrf\LB_stuff\Git\Road-Monitoring-on-Campus')
 from os.path import join, isdir, isfile
 from pathlib import PureWindowsPath
 import pandas as pd
 import tqdm
-from sklearn.neighbors import KDTree
-from data_pipeline import make_grid, capture_dense_optical_flow
-from detect_anomaly import visualize_dense_anomalous_gird
 
+from sklearn.neighbors import KDTree
+from data_pipeline import make_grid, capture_dense_optical_flow, grids_of_interest
+from detect_anomaly import visualize_dense_anomalous_gird
+import matplotlib.pyplot as plt
+import time 
 
 IF_TRAIN = False
 IF_VISUALIZE = True
@@ -47,26 +49,32 @@ print('working directory now: ', YOUR_PATH)
 
 
 # hyper parameters
-X_BLOCK_SIZE, Y_BLOCK_SIZE = 30,30
-TIME_INTERVAL = 5
+X_BLOCK_SIZE, Y_BLOCK_SIZE = 50,30
+TIME_INTERVAL = 20
 
-#train_video_path = (r'./data/QMUL/junction1/train/')
+#path_in = train_video_path = (r'./data/QMUL/junction1/train/')
 #test_video_path = (r'./data/QMUL/junction1/test/')
 #
 #sub_dir_train = [d[0:-4] for d in os.listdir(train_video_path) if isfile(join(train_video_path, d))]
 #sub_dir_test = [d[0:-4] for d in os.listdir(test_video_path) if isfile(join(test_video_path, d)) and not fnmatch.fnmatch(d, '*_gt')]
 #name_of_sub_dir = sub_dir_test[0]
+#'''
+#QMUL/test/028 is the u-turn video
+#050 is the reverse traffic
+#'''
+
+
 
 detect_results = (r'./data/visualized_results')
 
 
-# parameters for testing ucsd dataset
+## parameters for testing ucsd dataset
 UCSD_to_use = str(2)    
 
 # the input of the system is videos, set the path of the input videos 
 ucsd_train = (r'./data/UCSD/UCSDped'+UCSD_to_use+'/Train/')
 ucsd_test = (r'./data/UCSD/UCSDped'+UCSD_to_use+'/Test/')
-train_video_path = (r'./data/Train'+UCSD_to_use+'/videos/')
+path_in = train_video_path = (r'./data/Train'+UCSD_to_use+'/videos/')
 test_video_path = (r'./data/Test'+UCSD_to_use+'/videos/')
 
 
@@ -98,10 +106,84 @@ IDX_MAG_CAT = -1
 
 TEST_VIDEOS = -1
 
+#%% build maks of roi
+
+'''
+#%%
+gb = cv2.GaussianBlur(prev_gray, (5,5), 2)
+mb = cv2.medianBlur(prev_gray,5)
+can = cv2.Canny(gb, 30, 250)
+
+img = mask
+prev_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+mb = cv2.medianBlur(prev_gray,5)
+can = cv2.Canny(mb, 60, 220)
+plt.imshow(can)
+
+
+# should only use gray-scale img for thresholding
+
+ret,thresh1 = cv2.threshold(prev_gray,127,255,cv2.THRESH_BINARY)
+ret,thresh2 = cv2.threshold(prev_gray,127,255,cv2.THRESH_BINARY_INV)
+ret,thresh3 = cv2.threshold(prev_gray,127,255,cv2.THRESH_TRUNC)
+ret,thresh4 = cv2.threshold(prev_gray,127,255,cv2.THRESH_TOZERO)
+ret,thresh5 = cv2.threshold(prev_gray,127,255,cv2.THRESH_TOZERO_INV)
+
+th2 = cv2.adaptiveThreshold(mb,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+            cv2.THRESH_BINARY,11,2)
+th3 = cv2.adaptiveThreshold(mb,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,11,2)
+
+kernel = np.ones((5,5),np.uint8)
+erosion = cv2.erode(can,kernel,iterations = 1)
+dilation = cv2.dilate(can,kernel,iterations = 1)
+opening = cv2.morphologyEx(can, cv2.MORPH_OPEN, kernel)
+closing = cv2.morphologyEx(can, cv2.MORPH_CLOSE, kernel)
+gradient = cv2.morphologyEx(can, cv2.MORPH_GRADIENT, kernel)
+tophat = cv2.morphologyEx(can, cv2.MORPH_TOPHAT, kernel)
+blackhat = cv2.morphologyEx(can, cv2.MORPH_BLACKHAT, kernel)
+
+titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV',
+          'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding',
+          'erosion','dilation','opening','closing',
+          'gradient','tophat','blackhat']
+images = [mb, thresh1, thresh2, thresh3, thresh4, thresh5, th2, th3,
+          erosion, dilation, opening, closing, gradient, tophat, blackhat]
+
+
+
+for i in range(len(images)):
+    plt.figure(figsize=(40,28))
+    plt.subplot(5,3,i+1),plt.imshow(images[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+
+plt.show()
+
+1. ROI 抓不太到
+2. 嘗試針對右轉車道追蹤2秒前，其他的就設(0, 0)
+3. 太大的mag直接捨棄不要clip
+4. 長一點的time interval
+
+
+#%%
+
+testing = cv2.morphologyEx(th3, cv2.MORPH_GRADIENT, kernel).copy()
+plt.imshow(testing)
+#%%
+contours, hierarchy = cv2.findContours(testing, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+testing = cv2.drawContours(testing, contours, -1, (0,255,0), 15)
+
+plt.imshow(testing)
+'''
+
+
 #%%
 
 # make flow for all the videos in a directory
-def make_flow(path_of_videos, grids):
+def make_flow(path_of_videos, grids, ROI):
     
     '''
     Objective: capture optical flow points from video
@@ -118,11 +200,12 @@ def make_flow(path_of_videos, grids):
     sub_files = [d[0:-4] for d in os.listdir(path_of_videos) 
                 if isfile(join(path_of_videos, d)) 
                 and fnmatch.fnmatch(d, '*.avi')]
-    
+    sub_files = sub_files[0:51]
     flow_points = np.array([]).reshape(0, 7)
-    for d in tqdm.tqdm(sub_files[0:TEST_VIDEOS]):
+    for d in tqdm.tqdm(sub_files):
     #    frames_to_avi(ucsd_test, test_video_path, d, 10)
-        flow = capture_dense_optical_flow(path_of_videos, d, grids, TIME_INTERVAL, 
+        flow = capture_dense_optical_flow(path_of_videos, d, grids, 
+                                          ROI, TIME_INTERVAL, 
                                           X_BLOCK_SIZE, Y_BLOCK_SIZE,
                                           if_normal_ori = IF_NORMAL_ORI,
                                           if_show = False)
@@ -223,13 +306,13 @@ def kdtree_model(X_train, X_test, num_neighbor = 7, leaf_size = 40,
     
     return y_pred
 
-def make_train(path_of_videos, grids):
+def make_train(path_of_videos, grids, ROI):
 
     '''
     Objective: call the 'make_flow' and 'make_spatial_descriptor' to make training set
     '''
     
-    train_flow_points = make_flow(path_of_videos, grids)
+    train_flow_points = make_flow(path_of_videos, grids, ROI)
     
     std_magnitude = np.std(train_flow_points[:,IDX_MAGNITUDE])
     mag_med = np.median(train_flow_points[:,IDX_MAGNITUDE])
@@ -250,7 +333,7 @@ def make_train(path_of_videos, grids):
     
     return train_spatial, std_magnitude, mag_med
 
-def make_test(path_of_videos, grids, train_std_magnitude, train_mag_med):
+def make_test(path_of_videos, grids, ROI, train_std_magnitude, train_mag_med):
     
     
     '''
@@ -258,7 +341,7 @@ def make_test(path_of_videos, grids, train_std_magnitude, train_mag_med):
     '''
     
     magnitude_categories = 4
-    test_flow_points = make_flow(path_of_videos, grids)
+    test_flow_points = make_flow(path_of_videos, grids, ROI)
     
     mag_cat = np.array([(int(abs(m-train_mag_med)/train_std_magnitude)+1) 
                         if (abs(m-train_mag_med)<=train_std_magnitude*(magnitude_categories-1)) 
@@ -277,16 +360,23 @@ def make_test(path_of_videos, grids, train_std_magnitude, train_mag_med):
 #%%
 def main():
 
+    mask, ROI = grids_of_interest(train_video_path, GRIDS, X_BLOCK_SIZE, Y_BLOCK_SIZE, 
+                                  TIME_INTERVAL, if_show = True)
+
     if IF_TRAIN:
-        df_train_spatial, std_magnitude, mag_med =  make_train(train_video_path, GRIDS)
-        df_test_spatial = make_test(test_video_path, GRIDS, 
+        
+
+        df_train_spatial, std_magnitude, mag_med =  make_train(train_video_path, GRIDS, 
+                                                               ROI)
+        df_test_spatial = make_test(test_video_path, GRIDS, ROI,
                   std_magnitude, mag_med)
+
         
         X_train = df_train_spatial.iloc[:,0:-3]
         X_test = df_test_spatial.iloc[:,0:-3]
         video_info_test = df_test_spatial.loc[:,'grid': 'video'].astype(int)
         y_pred = kdtree_model(X_train, X_test, num_neighbor = 7, leaf_size = 40, 
-                     metric = 'l2', distance_threshold = 3.6)
+                     metric = 'l2', distance_threshold = 6)
         
     else:
         df_train_spatial = pd.read_csv(join((r'./data/Train'+UCSD_to_use+'/'), "df_train_dense_spatial"+UCSD_to_use+test_item+".csv"))
@@ -298,15 +388,15 @@ def main():
         X_test = df_test_spatial.iloc[:,0:-3]
         video_info_test = df_test_spatial.loc[:,'grid': 'video'].astype(int)
         y_pred = kdtree_model(X_train, X_test, num_neighbor = 7, leaf_size = 40, 
-                     metric = 'l2', distance_threshold = 3.6)
+                     metric = 'l2', distance_threshold = 3)
         
     if IF_VISUALIZE:    
         # for visualize anomalies
         for d in sub_dir_test[0:TEST_VIDEOS]:
     
             visualize_dense_anomalous_gird(test_video_path, d, GRIDS, y_pred, 
-                                       video_info_test, detect_results,
-                                       test_item, UCSD_to_use,
+                                       video_info_test, TIME_INTERVAL, detect_results,
+                                       test_item, UCSD_to_use, 
                                        X_BLOCK_SIZE , Y_BLOCK_SIZE,
                                        if_make_video = False)
 #%%
